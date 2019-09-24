5bbcbd4c8bced796e9709e0adc04ca883271d6ad
Adam Cooper <frankidyfrank3@gmail.com>
2019-09-24 14:28:34 -0500

    added comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,27 +2,35 @@ const data = require("../data/friends.js");
 
 
 module.exports = function (app) {
-
+    //if we recive a get request on the route /api/friends send the entire array of friends back
     app.get("/api/friends", function (req, res) {
         res.json(data)
     })
 
+    //if we recive a post request on the route /api/friends do the following
     app.post("/api/friends", function (req, res) {
+        //create the variables to be used to get the closest match based on the users inputted data
         let newData = req.body;
         let possibleMatch;
         let matchArr = [];
         let matchArrTwo = [];
         let forOne = 0;
         let forTwo = 0;
+        //loop through the users we already have
         data.forEach(person => {
+            //loop through the scores for the person
             person.scores.forEach((num, index) => {
+                //if the match array does not have a length of 10 then to the math and push to it, otherwise push to array 2
                 if (matchArr.length < 10) {
                     matchArr.push(Math.abs(num - newData.scores[index]));
                 } else {
                     matchArrTwo.push(Math.abs(num - newData.scores[index]));
                 }
             })
+
+            //if arr 2 has a length that means we have done the math on 2 separate people and must compare them
             if (matchArrTwo.length !== 0) {
+                //loop through both arrays to see what is bigger than the other and add a number for the appropriate array
                 matchArr.forEach((numb, index) => {
                     if (numb >= matchArrTwo[index]) {
                         forOne++;
@@ -30,10 +38,16 @@ module.exports = function (app) {
                         forTwo++;
                     }
                 });
+                //if for one is equal to or less than forTwo clear matchArrTwo, forTwo and forOne as for one is the closer match
                 if (forOne <= forTwo) {
+                    forOne = 0;
+                    forTwo = 0;
                     matchArrTwo = [];
                 } else {
-                    matchArr = [...matchArr];
+                    //set match arr to matchArrTwo as it is the better match
+                    forOne = 0;
+                    forTwo = 0;
+                    matchArr = [...matchArrTwo];
                     possibleMatch = person;
                     matchArrTwo = [];
                 }
@@ -41,6 +55,7 @@ module.exports = function (app) {
                 possibleMatch = person;
             }
         })
+        //add the new person to the array and send the data back for the closest possible match
         data.push(newData);
         res.json({status: 200, name: possibleMatch.name, photo: possibleMatch.photo});
     })
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+//importing express
 const express = require("express");
 const app = express();
 
+//requiring routes
 const htmlRoutes = require("./app/routing/htmlRoutes");
 const apiRoutes = require("./app/routing/apiRoutes.js");
 
+//setting the public folder to be visible to all people
 app.use(express.static(`${__dirname}/app/public`));
 app.use(express.json());
+
+//set the port
 const PORT = process.env.PORT || 3000;
 
+//run the routes
 apiRoutes(app);
 htmlRoutes(app);
 
+//listen to the port
 app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
 });
